72b32138a3c54f2f9221f6013ff89fb0ad608604
Ville Takanen <ville.takanen@futurice.com>
2023-01-04 10:17:52 +0200

    fix: added some species stats

--- src/composables/useCharacter/species.ts
+++ src/composables/useCharacter/species.ts
@@ -19,10 +19,15 @@ const elfSpecies:Record<string, CharacterFeature> = {
     type: 'species',
     effects: [
       ['size', 'Keskikokoinen'],
-      ['speed', '9 metriä'],
-      ['description', 'Pimeässä näkö 18 metriä'],
-      ['description', '*Keijujen perintö* Viholliset eivät saa etua hyökkäyksissäsi'],
-      ['description', '*Kevytuninen* Voit nukkua 4h päivässä'],
+      ['speed', '12 metriä'],
+      ['description', '*Pimeänäkö:* 24m'],
+      ['description', '*Tarkat aistit:* pätevyys tarkkaavaisuustaitoon'],
+      ['proficiency', 'perception'],
+      ['description', '*Keijujen perintö* sinua ei voi vaivuttaa uneen magialla, ja sinulla on etu pelastusheittoihin lumotuksi tulemista, kuten loitsua lumoa henkilö vastaan.'],
+      ['immunity', 'Unta vastaan'],
+      ['advantage', 'Lumoamista vastaan'],
+      ['description', '*Transsi* Transsi. Haltioiden ei tarvitse nukkua. Haltiat harjoittavat meditaatiota puoliksi tietoisessa tilassa neljä tuntia päivässä. '],
+      ['language', 'Yleiskieli'],
       ['language', 'Haltiakieli']
     ],
     variants: ['high_elf', 'wood_elf', 'dark_elf']
@@ -40,7 +45,21 @@ const elfSpecies:Record<string, CharacterFeature> = {
   dark_elf: {
     description: 'Pimeähaltia',
     type: 'species',
-    variantOf: 'elf'
+    variantOf: 'elf',
+    effects: [
+      ['size', 'Keskikokoinen'],
+      ['speed', '12 metriä'],
+      ['description', '*Ylivertainen pimeänäkö:* 48m'],
+      ['description', '*Tarkat aistit:* pätevyys tarkkaavaisuustaitoon'],
+      ['proficiency', 'perception'],
+      ['description', '*Keijujen perintö* sinua ei voi vaivuttaa uneen magialla, ja sinulla on etu pelastusheittoihin lumotuksi tulemista, kuten loitsua lumoa henkilö vastaan.'],
+      ['immunity', 'Unta vastaan'],
+      ['advantage', 'Lumoamista vastaan'],
+      ['description', '*Transsi* Transsi. Haltioiden ei tarvitse nukkua. Haltiat harjoittavat meditaatiota puoliksi tietoisessa tilassa neljä tuntia päivässä. '],
+      ['language', 'Yleiskieli'],
+      ['language', 'Haltiakieli'],
+      ['language', 'Aliskieli']
+    ],
   }
 }
 
