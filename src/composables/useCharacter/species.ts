d025d14c2946651297e61da3db327389d25750f1
Ville Takanen <ville.takanen@futurice.com>
2023-01-06 20:57:35 +0200

    fix: elves are in place

--- src/composables/useCharacter/species.ts
+++ src/composables/useCharacter/species.ts
@@ -41,12 +41,54 @@ const elfSpecies:Record<string, CharacterFeature> = {
   high_elf: {
     description: 'Jalohaltia',
     type: 'species',
-    variantOf: 'elf'
+    variantOf: 'elf',
+    effects: [
+      ['size', 'Keskikokoinen'],
+      ['speed', '12 metriä'],
+      ['description', '*Pimeänäkö:* 24m'],
+      ['description', '*Tarkat aistit:* pätevyys tarkkaavaisuustaitoon'],
+      ['proficiency', 'perception'],
+      ['description', '*Keijujen perintö* sinua ei voi vaivuttaa uneen magialla, ja sinulla on etu pelastusheittoihin lumotuksi tulemista, kuten loitsua lumoa henkilö vastaan.'],
+      ['immunity', 'Unta vastaan'],
+      ['advantage', 'Lumoamista vastaan'],
+      ['description', '*Transsi* Transsi. Haltioiden ei tarvitse nukkua. Haltiat harjoittavat meditaatiota puoliksi tietoisessa tilassa neljä tuntia päivässä. '],
+      ['language', 'Yleiskieli'],
+      ['language', 'Haltiakieli'],
+      ['proficiency', 'sword'],
+      ['proficiency', 'shortsword'],
+      ['proficiency', 'longbow'],
+      ['proficiency', 'shortbow'],
+    ],
+    options: [
+      {
+        feature: 'Kieli',
+        type: 'language',
+        values: [...allLanguages]
+      }
+    ]
   },
   wood_elf: {
     description: 'Metsähaltia',
     type: 'species',
-    variantOf: 'elf'
+    variantOf: 'elf',
+    effects: [
+      ['size', 'Keskikokoinen'],
+      ['speed', '16 metriä'],
+      ['description', '*Pimeänäkö:* 24m'],
+      ['description', '*Tarkat aistit:* pätevyys tarkkaavaisuustaitoon'],
+      ['proficiency', 'perception'],
+      ['proficiency', 'sword'],
+      ['proficiency', 'shortsword'],
+      ['proficiency', 'longbow'],
+      ['proficiency', 'shortbow'],
+      ['description', '*Keijujen perintö* sinua ei voi vaivuttaa uneen magialla, ja sinulla on etu pelastusheittoihin lumotuksi tulemista, kuten loitsua lumoa henkilö vastaan.'],
+      ['description', '*Sulaudu ympäristöön:* Voit yrittää piiloutua, vaikka näkyvyys sinuun olisi vain hieman heikentynyt lehtien, sumun, lumen tms. johdosta.'],
+      ['immunity', 'Unta vastaan'],
+      ['advantage', 'Lumoamista vastaan'],
+      ['description', '*Transsi* Transsi. Haltioiden ei tarvitse nukkua. Haltiat harjoittavat meditaatiota puoliksi tietoisessa tilassa neljä tuntia päivässä. '],
+      ['language', 'Yleiskieli'],
+      ['language', 'Haltiakieli']
+    ]
   },
   dark_elf: {
     description: 'Pimeähaltia',
