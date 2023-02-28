97219add435d954de3affd4dd1bd13309a405247
Ville Takanen <ville.takanen@futurice.com>
2023-02-28 14:16:36 +0200

    fix: repo structure fixes

--- src/root.tsx
+++ src/root.tsx
@@ -13,6 +13,23 @@ import {
   Title,
 } from "solid-start";
 import "./root.css";
+import '@11thdeg/cyan/style.css'
+import '@11thdeg/cyan'
+
+declare module 'solid-js' {
+  namespace JSX {
+    type ElementProps<T> = {
+      // Add both the element's prefixed properties and the attributes
+      [K in keyof T]: Props<T[K]> & HTMLAttributes<T[K]>;
+    }
+    // Prefixes all properties with `prop:` to match Solid's property setting syntax
+    type Props<T> = {
+      [K in keyof T as `prop:${string & K}`]?: T[K];
+    }
+    interface IntrinsicElements extends ElementProps<HTMLElementTagNameMap> {
+    }
+  }
+}
 
 export default function Root() {
   return (
@@ -22,7 +39,10 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Body>
+      <Body class="cyan cyan--themed cyan--mode--light">
+        <cyan-navigation-rail>
+          <cyan-navigation-button noun="ll" label="Hahmokone"></cyan-navigation-button>
+        </cyan-navigation-rail>
         <Suspense>
           <ErrorBoundary>
             <A href="/">Index</A>
